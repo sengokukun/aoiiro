10c1bc9faedceb0727c1fdc2ed2a2a4cdc38b083
(sengoku) <(a15dc095@dhw.ac.jp)>
2017-03-08 23:35:38 +0900

    メニュー追加

--- src/js/style.js
+++ src/js/style.js
@@ -4,13 +4,38 @@
 
 //tgglemenu
 $(function(){
-  var $toggleBtn = '.header-toggle';
-  var $triggers = '.toggle-lines';
-  $($toggleBtn).click(function() {
-    $($triggers).toggleClass('active');
-  });
-});
+  var menu = $('.master-navi'),
+      triggers = $('.toggle-lines'),
+      menuBtn = $('.header-toggle'),
+      body = $(document.body),
+      // .layer もオブジェクト化
+      layer = $('.layer'),
+      menuWidth = menu.outerWidth();
 
+      menuBtn.on('click', function(){
+      body.toggleClass('open');
+        if(body.hasClass('open')){
+          // css で非表示にしていた .layer を表示
+          $(".layer").show();
+          body.animate({'right' : menuWidth }, 300);
+          menu.animate({'right' : 0 }, 300);
+          $(triggers).addClass('active');
+        } else {
+          // .layer を非表示
+          $(".layer").hide();
+          menu.animate({'right' : -menuWidth }, 300);
+          body.animate({'right' : 0 }, 300);
+          $(triggers).removeClass('active');
+        }
+      });
+      // .layer をクリック時にもメニューを閉じる
+      layer.on('click', function(){
+        menu.animate({'right' : -menuWidth }, 300);
+        body.animate({'right' : 0 }, 300).removeClass('open');
+        $(triggers).removeClass('active');
+        layer.hide();
+      });
+});
 
 //TweenMax
 
