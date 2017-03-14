668728bcad89bcd87c06e5a44909949a3faafe9b
(sengoku) <(a15dc095@dhw.ac.jp)>
2017-03-15 03:12:30 +0900

    snsタイムライン追加

--- src/js/style.js
+++ src/js/style.js
@@ -63,7 +63,7 @@ window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.
 	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) return;
 	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.0";
+	js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.7";
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
